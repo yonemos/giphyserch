8cbef72d0ccfef759cff350e436bc04fd7f54fe5
komemo <msn_07zgok@yahoo.co.jp>
2018-10-14 22:56:28 +0900

    猫
    猫だけサーチできる状態

--- src/index.js
+++ src/index.js
@@ -2,14 +2,55 @@ import React from "react";
 import ReactDom from "react-dom";
 import axios from "axios";
 
-//function App() {
-//  return (
-//    <div className="App">
-//      <h1>Hello CodeSandbox</h1>
-//      <h2>Start editing to see some magic happen!</h2>
-//    </div>
-//  );
-//}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { gifUrlList: [] };
+  }
+  renderImageList(list) {
+    const imageList = list.map(url => {
+      return (
+        <li>
+          <img src={url} />
+        </li>
+      );
+    });
+    return <ul>{imageList}</ul>;
+  }
+  render() {
+    console.log(this.state.gifUrlList);
+    return <div>{this.renderImageList(this.state.gifUrlList)}</div>;
+  }
+  componentDidMount() {
+    this.giphyapi();
+  }
 
-const rootElement = document.getElementById("root");
-ReactDom.render(<App />, rootElement);
+  giphyapi() {
+    //リクエスト先のURLを作る
+    const search = "cat";
+    const key = "TjTZuEOFWOpvYl98kL6xJQfInMwnpS9z";
+    const limit = "10";
+    const url = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${key}&limit=${limit}`;
+
+    //axios request
+    axios.get(url).then(res => {
+      //dataを習得できた
+      console.log(res.data);
+      const data = res.data.data;
+
+      //img　のsrcに入れるためのURLにアクセスする
+      //const imageUrl = data[0].images.downsized.url;
+      //0番目だけじゃないものにする
+      const imageUrlList = data.map(item => item.images.downsized.url);
+      //console.log(imageUrl);
+      this.setState({ gifUrlList: imageUrlList });
+
+      //img要素を作って、Bodyに追加（reactではいらないので消）
+      //const img = document.createElement("img");
+      //img.src = imageUrl;
+      //document.body.appendChild(img);
+    });
+  }
+}
+
+ReactDom.render(<App />, document.getElementById("root"));
